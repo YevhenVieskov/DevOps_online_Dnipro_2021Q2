a66c423c001460da4f369256d636c3e0f0549f78
Yevhen Vieskov <evveskov@gmail.com>
2021-05-11 23:27:50 +0300

    task9 update 2

--- m9/calculator/calculator_tests.py
+++ m9/calculator/calculator_tests.py
@@ -1,12 +1,11 @@
 import unittest
-from Calculator import Calculator
-#Test cases to test Calulator methods
-#You always create  a child class derived from unittest.TestCase
+from calculator import Calculator
+
 class TestCalculator(unittest.TestCase):
-  #setUp method is overridden from the parent class TestCase
+  
   def setUp(self):
     self.calculator = Calculator()
-  #Each test method starts with the keyword test_
+  
   def test_add(self):
     self.assertEqual(self.calculator.add(4,7), 11)
   def test_subtract(self):
@@ -15,6 +14,6 @@ class TestCalculator(unittest.TestCase):
     self.assertEqual(self.calculator.multiply(3,7), 21)
   def test_divide(self):
     self.assertEqual(self.calculator.divide(10,2), 5)
-# Executing the tests in the above test case class
+
 if __name__ == "__main__":
   unittest.main()
--- m9/readme.md
+++ m9/readme.md
@@ -6,5 +6,6 @@
 
 # TASK 8.1
 
-Fizz_buzz program and unit tests: [fiz_buzz.py](fizz_buzz.py), [fizz_buzz_test.py](fiz_buzz_test.py)
-Calculator program and unit tests: [calculator.py](calculator.py), [calculator_test.py](calculator_test.py)
+Fizz_buzz program and unit tests: [fiz_buzz.py](./fizz_buzz/fizz_buzz.py), [fizz_buzz_test.py](./fizz_buzz/fiz_buzz_test.py)
+Calculator program and unit tests: [calculator.py](./calculator/calculator.py), [calculator_test.py](./calculator/calculator_test.py)
+Count vowels program and unit tests: [count_vowels.py](./calculator/count_vowels.py), [count_vowels_test.py](./calculator/count_vowels_test.py)
